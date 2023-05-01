585a41a27ce1d4a991396a271c619b695e184fba
Jason Rivalino <jasonrivalino@gmail.com>
2023-05-01 14:35:59 +0700

    feat: add frontend feature

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,6 +3,8 @@ import './styles/LandingPage.css';
 import React, { useState } from "react";
 import ChatList from "./components/ChatList";
 import ChatInput from "./components/ChatInput";
+import "./styles/ChatHistory.css";
+import RadioButton from './components/RadioButton';
 
 function App() {
   const [messages, setMessages] = useState([]);
@@ -13,8 +15,20 @@ function App() {
   return (
     <div className="myBG">
     <div className="container">
+      <div className='chat-history'></div>
+      <h1 className="title">ChatterBot-STIM(AI)</h1>
+      <h2 className="history">History</h2>
+      <h3 className="algorithms">Algoritma:</h3>
       <ChatList messages={messages} />
       <ChatInput addMessage={addMessage} />
+      <RadioButton
+        name="algoritma"
+        value1="0"
+        label1="   Knuth-Moore-Pratt (KMP)"
+        value2="1"
+        label2="   Boyer-Moore (BM)"
+        defaultValue="0"
+      />
     </div>
     </div>
   );
---
+++ src/frontend/src/components/RadioButton.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import '../styles/RadioButton.css';
+
+function RadioButton(props) {
+  const [selectedValue, setSelectedValue] = useState(props.defaultValue);
+
+  function handleChange(event) {
+    setSelectedValue(event.target.value);
+  }
+
+  return (
+    <div className='radioButton'>
+
+      <label>
+        <input
+          type="radio"
+          name={props.name}
+          value={props.value1}
+          checked={selectedValue === props.value1}
+          onChange={handleChange}
+        />
+        {props.label1}
+      </label>
+
+      <br/><br/>
+      
+      <label>
+        <input
+          type="radio"
+          name={props.name}
+          value={props.value2}
+          checked={selectedValue === props.value2}
+          onChange={handleChange}
+        />
+        {props.label2}
+      </label>
+    
+    </div>
+  );
+}
+
+export default RadioButton;
